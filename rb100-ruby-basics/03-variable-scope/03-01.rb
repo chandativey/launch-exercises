645e16647098e633467a603c647e568a97c88823
Jessie Tivey <jesstivey@gmail.com>
2020-04-23 17:42:24 -0400

    Completed 3/10 exercises from Variable Scope

---
+++ rb100-ruby-basics/03-variable-scope/03-01.rb
@@ -0,0 +1,14 @@
+# What's My Value? (Part 1)
+
+# Q: What will the following code print and why? Don't run it until you have tried to answer.
+
+a = 7
+
+def my_value(b)
+	b += 10
+end
+
+my_value(a)
+puts a
+
+# A: 7
