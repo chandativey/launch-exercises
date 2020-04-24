d2d3504aa1d27e6242457dbd4266a3dd156f1702
Jessie Tivey <jesstivey@gmail.com>
2020-04-24 11:56:57 -0400

    Completed 5/10 exercises from Loops 1

---
+++ rb100-ruby-basics/04-loops-1/04-04-loop-on-command.rb
@@ -0,0 +1,9 @@
+# Loop on Command
+
+# Modify the code below so the loop stops iterating when the user inputs 'yes'. 
+
+loop do
+	puts 'Should I stop looping?'
+	answer = gets.chomp.downcase
+	break if answer == 'yes'
+end
---
+++ rb100-ruby-basics/04-loops-1/04-05-say-hello.rb
@@ -0,0 +1,12 @@
+# Say Hello
+
+# Modify the code below so "Hello!" is printed 5 times.
+
+say_hello = true
+i = 0
+
+while say_hello
+	puts 'Hello!'
+	i += 1
+	say_hello = false if i == 5
+end
