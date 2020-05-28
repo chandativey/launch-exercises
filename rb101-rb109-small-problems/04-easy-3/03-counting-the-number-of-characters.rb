675c4d307057e622886acfb61f1538c3e236f7e6
Jessie Tivey <jesstivey@gmail.com>
2020-05-28 08:55:57 -0400

    Completed 3/10 exercises for Easy 3

---
+++ rb101-rb109-small-problems/04-easy-3/03-counting-the-number-of-characters.rb
@@ -0,0 +1,8 @@
+# Counting the Number of Characters
+
+# Write a program that will ask a user for an input of a word or multiple words and give back the number of characters. Spaces should not be counted as a character.
+
+puts "Please write word or multiple words:"
+word = gets.chomp
+
+puts "There are #{word.delete(' ').size} characters in \"#{word}\"."
