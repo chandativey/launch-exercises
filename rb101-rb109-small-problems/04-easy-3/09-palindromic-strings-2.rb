c8b13c59e399512648e080deb53e8ca416347cfd
Jessie Tivey <jesstivey@gmail.com>
2020-05-28 20:55:19 -0400

    Completed 9/10 exercises for Easy 3

---
+++ rb101-rb109-small-problems/04-easy-3/09-palindromic-strings-2.rb
@@ -0,0 +1,12 @@
+# Palindromic Strings (Part 2)
+
+# Write another method that returns true if the string passed as an argument is a palindrome, false otherwise. This time, however, your method should be case-insensitive, and it should ignore all non-alphanumeric characters. If you wish, you may simplify things by calling the `palindrome?` method you wrote in the previous exercise.
+
+def palindrome?(str)
+  str == str.reverse
+end
+
+def real_palindrome?(str)
+  str = str.downcase.delete('^a-z0-9')
+  palindrome?(str)
+end
