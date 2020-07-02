c90d1c6be438d74e1e3447ce82fa00d63db36d34
Jessie Tivey <jesstivey@gmail.com>
2020-07-02 10:03:27 -0400

    Completed exercises in Reading Documentation

---
+++ rb100-ruby-basics/2nd-time/reading-documentation-2/03.rb
@@ -0,0 +1,8 @@
+# Optional Arguments
+
+# Assume you have the following code. What will each of the 3 puts statements print?
+
+s = 'abc def ghi,jkl mno pqr,stu vwx yz'
+puts s.split.inspect
+puts s.split(',').inspect
+puts s.split(',', 2).inspect
