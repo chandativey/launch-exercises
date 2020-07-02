41f0e21a44895390ca74b787f2c4a11d5c7ed17e
Jessie Tivey <jesstivey@gmail.com>
2020-07-02 10:10:43 -0400

    Completed exercises in Reading Documentation 2

--- rb100-ruby-basics/2nd-time/reading-documentation-2/03.rb
+++ rb100-ruby-basics/2nd-time/reading-documentation-2/03.rb
@@ -3,6 +3,6 @@
 # Assume you have the following code. What will each of the 3 puts statements print?
 
 s = 'abc def ghi,jkl mno pqr,stu vwx yz'
-puts s.split.inspect
-puts s.split(',').inspect
-puts s.split(',', 2).inspect
+puts s.split.inspect            # ["abc", "def", "ghi,jkl", "mno", "pqr,stu", "vwx", "yz"]
+puts s.split(',').inspect       # ["abc def ghi", "jkl mno pqr", "stu vwx yz"]
+puts s.split(',', 2).inspect    # ["abc def ghi", "jkl mno pqr,stu vwx yz"]
