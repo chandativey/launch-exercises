51d738c46c70c85621391d7af4fba70e857392a9
Jessie Tivey <jesstivey@gmail.com>
2020-07-02 10:19:34 -0400

    Completed 1/10 exercises in Variable Scope

---
+++ rb100-ruby-basics/2nd-time/variable-scope/01.rb
@@ -0,0 +1,15 @@
+# What's my Value? (Part 1)
+
+# What will the following code print and why? Don't run it until you have tried to answer.
+
+a = 7
+
+def my_value(b)
+  b += 10
+end
+
+my_value(a)
+puts a
+
+# 7
+# Reassignment doesn't mutate a variable. Additionally, numbers are immutable.
