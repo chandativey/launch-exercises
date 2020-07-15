c5d8769cb71cbe39dd8696ca29d7643cc15c33b9
Jessie Tivey <jesstivey@gmail.com>
2020-07-15 14:25:15 -0400

    Completed 10/10 exercises from Methods

---
+++ rb100-ruby-basics/2nd-time/methods/03.rb
@@ -0,0 +1,13 @@
+# Greeting Through Methods (Part 1)
+
+# Write two methods, one that returns the string "Hello" and one that returns the string "World". Then print both strings using #puts, combining them into one sentence.
+
+def str1
+  "Hello"
+end
+
+def str2
+  "World"
+end
+
+puts "#{str1} #{str2}"
---
+++ rb100-ruby-basics/2nd-time/methods/04.rb
@@ -0,0 +1,17 @@
+# Greeting Through Methods (Part 2)
+
+# Write a method named greet that invokes the following methods:
+
+def str1
+  "Hello"
+end
+
+def str2
+  "World"
+end
+
+def greet
+  "#{str1} #{str2}"
+end
+
+puts greet
