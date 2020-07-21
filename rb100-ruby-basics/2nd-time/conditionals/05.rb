7d2aa2f60eb92b11d5d1bf50a4ec01dde137126b
Jessie Tivey <jesstivey@gmail.com>
2020-07-21 09:27:46 -0400

    Completed 10/10 exercises in Conditionals

---
+++ rb100-ruby-basics/2nd-time/conditionals/05.rb
@@ -0,0 +1,13 @@
+# Truthy Number
+
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+# This is going to print "My favorite number is 7." All expressions are true unless explicitly declared false or nil. 
