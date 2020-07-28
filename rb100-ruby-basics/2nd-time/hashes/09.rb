16bb07a6644f09140b8ca31ce1bc1a86b7a8042f
Jessie Tivey <jesstivey@gmail.com>
2020-07-28 17:19:08 -0400

    Completed 10/10 exercises in Hashes

---
+++ rb100-ruby-basics/2nd-time/hashes/09.rb
@@ -0,0 +1,8 @@
+# Multiple Cars
+
+# Create a nested hash using the following data.
+
+{
+  car: { type: "sedan", color: "blue", year: 2003 },
+  truck: { type: "pickup", color: "red", year: 1998 }
+}
