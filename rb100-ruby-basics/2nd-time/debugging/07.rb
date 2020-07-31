770abf7ca5734c58b027fe430887af74b88d3383
Jessie Tivey <jesstivey@gmail.com>
2020-07-31 10:31:37 -0400

    Completed 10/10 exercises in Debugging

--- rb100-ruby-basics/2nd-time/debugging/07.rb
+++ rb100-ruby-basics/2nd-time/debugging/07.rb
@@ -9,7 +9,7 @@ balance = 0
 # Here's what you earned and spent during the first three months.
 
 january = {
-  income: [ 1200, 7,5 ]
+  income: [ 1200, 75 ],
   expenses: [ 650, 140, 33.2, 100, 26.9, 78 ]
 }
 
@@ -23,17 +23,19 @@ march = {
   expenses: [ 650, 140, 350, 12, 59.9, 2.5 ]
 }
 
-# Let's see how much you've got now
+# Let's see how much you've got now...
 
 def calculate_balance(month)
-  plus = month[:income].sum
+  plus  = month[:income].sum
   minus = month[:expenses].sum
 
   plus - minus
 end
 
 [january, february, march].each do |month|
-  balance = calculate_balance(month)
+  balance += calculate_balance(month)
 end
 
 puts balance
+
+# We were incorrectly reassigning balance to equal the last evaluated month instead of adding and reassigning. 
---
+++ rb100-ruby-basics/2nd-time/debugging/09.rb
@@ -0,0 +1,20 @@
+# Digit Product
+
+# Given a String of digits, our digit_product method should return the product of all digits in the String argument. You've been asked to implement this method without using reduce or inject.
+
+# When testing the method, you are surprised by a return value of 0. What's wrong with this code and how can you fix it?
+
+def digit_product(str_num)
+  digits = str_num.chars.map { |n| n.to_i }
+  product = 1
+
+  digits.each do |digit|
+    product *= digit
+  end
+
+  product
+end
+
+p digit_product('12345')
+
+# Product being assigned to 0 means that anything multiplied by it is 0, which is why we get 0 as result. We can change product to equal 1 and we get the expected result.
